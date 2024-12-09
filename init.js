bb2f0f4e43eafadfb0210cc316331e9c128f0bfa
时瑾 <74231782+sj817@users.noreply.github.com>
2024-12-09 23:18:27 +0800

    fix: pm2

--- init.js
+++ init.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-function findProjectRoot (startDir) {
+const findProjectRoot = (startDir) => {
   let dir = startDir
   while (dir !== path.parse(dir).root) {
     if (fs.existsSync(path.join(dir, 'package.json'))) {
@@ -16,23 +16,23 @@ function findProjectRoot (startDir) {
 }
 
 const projectRoot = process.env.INIT_CWD || findProjectRoot(process.cwd())
-const file = path.join(projectRoot, 'package.json')
-/** 获取当前文件的路径 */
-const filePath = fileURLToPath(import.meta.url)
-
-/** 如果不处于npm包环境 不修改 */
-if (filePath.includes('node_modules')) {
-  const main = './' + path.relative(projectRoot, path.join(path.dirname(filePath), './lib/index.js')).replace(/\\/g, '/')
-  const pkg = JSON.parse(fs.readFileSync(file, 'utf-8'))
-  pkg.type = 'module'
-  pkg.main = main
-  pkg.types = main.replace(/js$/, 'd.ts')
-  if (!pkg.scripts) pkg.scripts = {}
-  pkg.scripts.start = 'node ' + main
-  /** 延迟1秒写入 */
-  setTimeout(() => {
-    fs.writeFileSync(file, JSON.stringify(pkg, null, 2))
-  }, 1000)
-  fs.writeFileSync(path.join(projectRoot, 'index.js'), `import('@karinjs/puppeteer')`)
+/** 处于postinstall脚本环境 无法修改package.json */
+if (process.env.INIT_CWD) {
+  fs.writeFileSync(path.join(projectRoot, 'index.js'), `import('@karinjs/puppeteer')\n`)
   console.log('初始化完成，请使用【node .】启动~')
+} else {
+  const file = process.cwd() + '/package.json'
+  /** 获取当前文件的路径 */
+  const filePath = fileURLToPath(import.meta.url)
+
+  /** 如果不处于npm包环境 不修改 */
+  if (filePath.includes('node_modules')) {
+    const pkg = JSON.parse(fs.readFileSync(file, 'utf-8'))
+    pkg.type = 'module'
+    if (!pkg.scripts) pkg.scripts = {}
+    fs.writeFileSync(file, JSON.stringify(pkg, null, 2))
+  }
+
+  console.log('初始化完成，请使用【node .】启动~')
+  console.log('tips: 全部功能请使用【npx k】查看')
 }
